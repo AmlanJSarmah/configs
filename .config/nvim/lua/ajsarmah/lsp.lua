f42b34e3b8cb2d8485f40b1843cdadacc70315cd
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-03-14 08:36:30 +0530

    Added LSP for Javascript

--- .config/nvim/lua/ajsarmah/lsp.lua
+++ .config/nvim/lua/ajsarmah/lsp.lua
@@ -2,10 +2,11 @@ require('lsp-zero')
 require('lspconfig').lua_ls.setup({})
 require('lspconfig').clangd.setup({})
 require('lspconfig').pyright.setup({})
+require('lspconfig').tsserver.setup({})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'lua_ls', 'pyright', 'clangd'},
+  ensure_installed = {'lua_ls', 'pyright', 'clangd', 'tsserver'},
 })
 
 local cmp = require('cmp')
