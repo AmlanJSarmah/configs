db0e1c9be5a80c96cf9dae6c903376d2344695a1
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-03-18 21:27:54 +0530

    Added LSP for Go and Treesitter support for Haskel

--- .config/nvim/lua/ajsarmah/lsp.lua
+++ .config/nvim/lua/ajsarmah/lsp.lua
@@ -2,12 +2,15 @@ require('lsp-zero')
 require('lspconfig').lua_ls.setup({})
 require('lspconfig').clangd.setup({})
 require('lspconfig').pyright.setup({})
+-- requires recent version of node and npm in stable distros use nvm
 require('lspconfig').tsserver.setup({})
 require('lspconfig').gopls.setup({})
+-- We need to install the lsp for haskell externally using ghcup, if ghcup isn't install COMMENT OUT THE LINE BELOW !
+require('lspconfig').hls.setup({})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'lua_ls', 'pyright', 'clangd', 'tsserver', 'gopls'},
+  ensure_installed = {'lua_ls', 'pyright', 'clangd', 'tsserver', 'gopls', 'hls'},
 })
 
 local cmp = require('cmp')
